de9ed32271f9cf152bbd762165940991c486e033
orenmagid <autoimpedicis@gmail.com>
2018-11-08 15:25:42 -0500

    Added Color

--- src/Task.js
+++ src/Task.js
@@ -3,13 +3,35 @@ import React, { Component } from "react";
 export default class Task extends Component {
   render() {
     const { task, moveTask, index, lastIndex, onDrag } = this.props;
+
+    let color;
+    switch (index) {
+      case 0:
+        color = "teal";
+        break;
+      case 1:
+        color = "blue";
+        break;
+      case 2:
+        color = "violet";
+
+        break;
+
+      case 3:
+        color = "purple";
+        break;
+      default:
+        color = "orange";
+    }
+
+    let style = `ui centered ${color} segment`;
     return (
       <div
         className="item"
         draggable
         onDrag={event => onDrag(event, task, index)}
       >
-        <div className="ui centered segment">
+        <div className={style}>
           {index === 0 ? (
             <i
               onClick={() => moveTask(task, index, "delete")}
